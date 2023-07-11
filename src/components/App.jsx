f9eeb565ff6b395cd315ee822263f3c0b31e38ff
NataliiaAndreieva <taliandreeva@gmail.com>
2023-07-11 18:52:04 +0300

    done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,96 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+import React, { Component} from "react";
+import { nanoid } from 'nanoid';
+
+import Form from "./Form";
+import Filter from "./Filter";
+import ContactsList from "./ContactsList";
+import { Container } from "./App.styled";
+
+export class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+  };
+
+  addNewContact = data => {
+    const { contacts } = this.state;
+    const contactExist = contacts.some(
+      ({ name }) => name.toLocaleLowerCase() === data.name.toLocaleLowerCase()
+    );
+
+    if (contactExist) return alert(`${data.name} is alredy in contacts`);
+
+    const newContact = { id: nanoid(), ...data };
+
+      this.setState(prevState => ({
+        contacts: [...prevState.contacts, newContact],
+      }));
+  };
+  
+  changeFilter = e => {
+    this.setState({ filter: e.currentTarget.value });
+  };
+
+  getVisibleContacts = () => {
+    const { contacts, filter } = this.state;
+    const normalizedFilter = filter.toLowerCase();
+
+    return contacts.filter(({name}) =>
+      name.toLowerCase().includes(normalizedFilter)
+    );
+  };
+
+  deleteContact = id => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== id),
+    }));
+  };
+
+  componentDidMount() {
+    console.log('App Mount');
+
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  };
+
+  componentDidUpdate(prevProps, prevState) {
+    console.log('app');
+
+    if (this.state.contacts !== prevState.contacts)
+      console.log('оновилося поле');
+    
+    localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+  };
+
+  render() {
+console.log('app render')
+
+    const { filter } = this.state;
+    const visibleContacts = this.getVisibleContacts();
+    return (
+      <>
+        <Container>
+          <h1>Phonebook</h1>
+          <Form addNewContact={this.addNewContact } />
+          <h2>Contacts</h2>
+          <Filter value={filter} onChange={this.changeFilter} />
+          <ContactsList
+            contacts={visibleContacts}
+            deleteContact={this.deleteContact}
+          />
+        </Container>
+      </>
+    );
+  }
 };
+
+export default App;
---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,68 @@
+import React, { Component } from "react";
+import { Container, Button } from "./Form.styled";
+
+class Form extends Component {
+  state = {
+    name: '',
+    number: '',
+  };
+
+  handleInputChange = event => {
+    const { name, value } = event.currentTarget;
+    this.setState({ [name]: value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    const { addNewContact } = this.props;
+    addNewContact({...this.state});
+    
+      this.reset();
+    };
+
+    reset = () => {
+      this.setState({ name: '', number: '' });
+    }
+    
+  render() {
+    return (
+      <Container onSubmit={this.handleSubmit}>
+        <label>
+          Name
+          <input
+            type="text"
+            name="name"
+            pattern="^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+            value={this.state.name}
+            onChange={this.handleInputChange}
+          />
+        
+        </label>
+
+        <label>
+          Number
+          <input
+            onChange={this.handleInputChange}
+            type="tel"
+            name="number"
+            value={this.state.number}
+            placeholder="000-00-00"
+            pattern='\+?\d{1,4}?[ .\-\s]?\(?\d{1,3}?\)?[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,9}'
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            required
+          />
+        </label>
+        <Button
+          type="submit"
+          // disabled={!this.state.name || !this.state.number}
+        >
+          Add contact
+        </Button>
+      </Container>
+    );
+  }
+}
+
+export default Form;
